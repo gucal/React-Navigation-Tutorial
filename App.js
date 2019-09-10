67254ff68588fda8e7fdd1c3a0d1012de4e35116
Gucal <gucalaliriza@outlook.com>
2019-09-10 15:58:16 +0300

    Division

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
-import {createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
+import Router from './Navigation/Router'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <AppStackNavigator />
-    );
+        <Router />
+    )
   }
 }
-const AppStackNavigator = createStackNavigator({
-  Login: LoginScreen,
-  Home: HomeScreen
-});
-
-const AppNavigator = createAppContainer(AppStackNavigator);
-export default AppNavigator;
---
+++ Navigation/Router.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import {createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+
+class Router extends Component {
+    render() {
+        return (
+            <AppNavigator />
+        )
+    }
+}
+const AppStackNavigator = createStackNavigator({
+    Login: LoginScreen,
+    Home: HomeScreen
+  });
+  
+  const AppNavigator = createAppContainer(AppStackNavigator);
+  export default AppNavigator;
